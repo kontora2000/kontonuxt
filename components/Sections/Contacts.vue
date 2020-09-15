75cf1809f45fdd28efb97d96aa0c0173c2752edc
stasmyhero <stasmyhero@gmail.com>
2020-09-15 19:16:18 +0300

    bugfix with contacts

---
+++ components/Sections/Contacts.vue
@@ -0,0 +1,34 @@
+<template>
+         <section class="big-contacts-cont">
+            <h1 class="mainpage-section-header">Если захотели <br class="only-desktop"/>с&nbsp;нами связаться&nbsp;:)</h1>
+            <div class="big-contacts-inner">
+                <div class="tel-email-wrapper">
+                    <span>+7&nbsp;988&nbsp;668-14-88</span>
+                    <a class="link-email link-underline-solid" href="mailto:mail@kontora.cc">mail@kontora.cc</a>
+                </div>
+                <div class="social-links-wrapper">
+                    <div class="social-link-wrapper">
+                        <a class="social-link" href="#">
+                            <svg class="icon-svg icon-telegram-svg">
+                                <use xlink:href="sprite.svg#icon-telegram" />
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="social-link-wrapper">
+                        <a class="social-link" href="#">
+                            <svg class="icon-svg icon-whatsapp-svg">
+                                <use xlink:href="sprite.svg#icon-whatsapp" />
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="social-link-wrapper">
+                        <a class="social-link" href="#">
+                            <svg class="icon-svg icon-instagram-svg">
+                                <use xlink:href="sprite.svg#icon-instagram" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
