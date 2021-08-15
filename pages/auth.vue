6f87a38e4ef5fb2d81adf5dd6e06a8d5e42b824c
stasmyhero <49776649+stasmyhero@users.noreply.github.com>
2021-08-15 17:59:03 +0300

    implemented auth page

---
+++ pages/auth.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input v-model="login" name="login" type="text">
+    <input v-model="pass" name="password" type="password">
+    <button>
+      Авторизоваться
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      login: '',
+      pass: '',
+    };
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        await this.store.dispatch('auth/login');
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+};
+</script>
